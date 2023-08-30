238592802139c67a58cfc3129aa8eb1ad68e0489
adrifallas12 <143028706+adrifallas12@users.noreply.github.com>
2023-08-30 15:52:54 -0600

    Update streamlit_app.py
    added some options to the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('kale')
+streamlit.text('tomato')
+streamlit.text('avocado')
+streamlit.text('eggs')
