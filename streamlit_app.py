1c1073f0ba914e1eaa42921fb345c6035042f785
adrifallas12 <143028706+adrifallas12@users.noreply.github.com>
2023-08-31 15:03:34 -0600

    streamlit_app.py
    added the option for user enter the option

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #--------
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Frutyvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
@@ -43,4 +45,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #show it in streamlit
 streamlit.dataframe(fruityvice_normalized)
+
+#new section to show frutyvice API response
 
